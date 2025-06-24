680a0bf792c77f2daa8eab1e9cffa961b8bad105
Rodney <rodney.amani@student.moringaschool.com>
2025-06-24 04:23:45 +0300

    changes

--- vite-project/src/pages/Login.jsx
+++
@@ -1,4 +0,0 @@
-function Login() {
-  return <h2 className="text-2xl font-semibold text-center">Login Page</h2>;
-}
-export default Login;
--- vite-project/src/pages/Register.jsx
+++
@@ -1,4 +0,0 @@
-function Register() {
-  return <h2 className="text-2xl font-semibold text-center">Register Page</h2>;
-}
-export default Register;
